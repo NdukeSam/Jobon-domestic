c57607e47403af07c90a28dd9d45ba9f6a66c44d
Nduke Sam <diamondsam33@gmail.com>
2024-06-03 01:22:08 +0100

    landing

--- employee_ui/src/components/Navbar.jsx
+++ employee_ui/src/components/Navbar.jsx
@@ -5,15 +5,21 @@ const Navbar = () => {
     <div>
       <nav className="top-0 z-50 py-3">
         <div className="container px-4 mx-auto relative text-sm">
-            <div className="flex justify-between items-center">
+            <div className="flex justify-between items-center ">
                 <div className="logo flex items-center flex-shrink-0">
-                    <span className="text-xl font-bold">JobOn</span>
+                    <span className=" ml-10 text-xl font-bold">JobOn</span>
                 </div>
-                <ul className="hidden lg:flex ml-14 space-x-12">
+                <ul className="hidden lg:flex font-semibold space-x-12 mr-10">
                     <li>
                         <a href="#">About</a>
+                    </li>
+                    <li>
                         <a href="#">Policy</a>
+                    </li>
+                    <li>
                         <a href="#">Contact</a>
+                    </li>
+                    <li>
                         <a href="#">Login</a>
                     </li>
                 </ul>
